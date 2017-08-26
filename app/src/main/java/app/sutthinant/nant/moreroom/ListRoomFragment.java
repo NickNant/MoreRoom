f12c7f555233ff001ef03837961853e1be65ff3e
nant <sutthinant@gmail.com>
2017-08-26 14:24:53 +0700

    Get JSON OK

--- app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
+++ app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by NantHome on 8/26/2017.
@@ -27,7 +29,31 @@ public class ListRoomFragment extends Fragment{
         //Create ToolBar
         createToolBar();
 
-    }
+        //Create ListView
+        createListView();
+
+
+    }   //on Activity Create
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livRoom);
+        MyConstant myConstant = new MyConstant();
+        String strURL = myConstant.getUrlGetRoomString();
+        String tag = "26AugV1";
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(strURL);
+            String strJSON = getAllData.get();
+            Log.d(tag, "JSON ==>" + strJSON);
+
+
+        } catch (Exception e) {
+            Log.d(tag, "e creatListView ==>" + e.toString());
+        }
+
+    }  // Create ListView
 
     private void createToolBar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarListRoom);
