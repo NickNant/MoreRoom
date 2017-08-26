64e6ffd91c2c0815e419015e4bb997d683a7d621
nant <sutthinant@gmail.com>
2017-08-26 13:28:20 +0700

    ListRoom Fragment OK

---
+++ app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
@@ -0,0 +1,21 @@
+package app.sutthinant.nant.moreroom;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by NantHome on 8/26/2017.
+ */
+
+public class ListRoomFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_listroom, container, false);
+        return view;
+    }
+}   //Main class
